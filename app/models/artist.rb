bb340df45c3dcd5898e1af1d2bceb72c21569133
keeks5456 <Akikogreen08@gmail.com>
2020-06-27 14:11:29 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    self.songs.count 
+  end
+  # dont forget that artist has a connecton to songs and thats why we can say self.songs.count
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+    artist1 = Artist.create(name: "Sting")
+    artist2 = Artist.create(name: "Seal")
+    artist3 = Artist.create(name: "Kanye")
+
+    song1= Song.create(title: "sting song1", artist_id: artist1.id)
+    song2= Song.create(title: "kissed by a rose", artist_id: artist2.id)
+    song3= Song.create(title: "kanye stuff here", artist_id: artist3.id)
